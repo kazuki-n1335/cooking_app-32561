08240416ed78f2316049c0d75eb77ba278fa6e71
kazuki-n1335 <kazuki.240.1337@gmail.com>
2021-01-11 12:40:06 +0900

    ビュー調整など

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -32,7 +32,7 @@ class RecipesController < ApplicationController
   def destroy
     @recipe.destroy
     if @recipe.destroy
-      redirect_to root_path
+      redirect_to user_path(current_user.id)
     else
       render :show
     end
@@ -44,9 +44,10 @@ class RecipesController < ApplicationController
 
   def update
     tag_list = params[:recipe][:tag].split(",")
-    if @recipe.save
+    if @recipe.valid?
+      @recipe.update(recipe_params)
       @recipe.save_tags(tag_list)
-      redirect_to root_path
+      redirect_to user_path(current_user.id)
     else
       render :edit
     end
