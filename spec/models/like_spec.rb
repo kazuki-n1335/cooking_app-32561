41bf557d76866d0bc24253e557f35bbe384429ed
kazuki-n1335 <kazuki.240.1337@gmail.com>
2021-01-12 11:41:55 +0900

    単体モデルテスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+  describe 'お気に入り登録' do
+    context '新規登録がうまくいくとき' do
+      it '正常に記入すると新規登録できる' do
+        expect(@like).to be_valid
+      end
+    end
+  
+    context '新規登録がうまくいかないとき' do
+      it 'userが紐付いていないと保存できないこと' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include('ユーザーを入力してください')
+      end
+      it 'recipeが紐付いていないと保存できないこと' do
+        @like.recipe = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include('レシピを入力してください')
+      end
+      it '同じユーザーが同じレシピをお気に入り登録できないこと' do
+        @like.save
+        another_like = FactoryBot.build(:like)
+        another_like.recipe = @like.recipe
+        another_like.user = @like.user
+        another_like.valid?
+        expect(another_like.errors.full_messages).to include('Userはすでに存在します')
+      end
+    end
+  end
 end
