0ae78c5a72d6558a7f17d21eecb288ff7a36b1c4
kazuki-n1335 <kazuki.240.1337@gmail.com>
2021-01-13 19:44:58 +0900

    投稿画像プレビュー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @stocks = @user.stocks.order("category_id")
     @shoppings = @user.shoppings.order("category_id")
-    @recipes = @user.recipes.includes(recipe_tags: []).where(release: 1).order("created_at DESC").page(params[:page]).per(8)
-    @myrecipes = @user.recipes.includes(recipe_tags: []).order("created_at DESC").page(params[:user_recipe]).per(6)
+    @recipes = @user.recipes.includes(recipe_tags: []).where(release: 1).order("updated_at DESC").page(params[:page]).per(8)
+    @myrecipes = @user.recipes.includes(recipe_tags: []).order("updated_at DESC").page(params[:user_recipe]).per(6)
     @plans = @user.plans.includes(:recipe).order("date")
     likes = Like.where(user_id: current_user.id).pluck(:recipe_id)
-    @like_recipes = Recipe.where(id: likes).includes(recipe_tags: []).order("created_at DESC").page(params[:recipe_like]).per(6)
+    @like_recipes = Recipe.where(id: likes).includes(recipe_tags: []).order("updated_at DESC").page(params[:recipe_like]).per(6)
   end
 end
